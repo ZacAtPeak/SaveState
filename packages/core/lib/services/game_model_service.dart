0f0bfac436cdf6ff2494e38b257186a1b52baad1
ZacAtPeak <105678604+ZacAtPeak@users.noreply.github.com>
2026-05-08 08:43:30 -0600

    feat(06-02): create GameModelService ChangeNotifier with rootBundle loading
    - Create game_model_service.dart with ChangeNotifier, activeModel/isLoaded getters
    - loadFromAsset() loads JSON via rootBundle, parses with GameModelParser
    - Export in services.dart barrel
    - Fix: added missing GameModel import

---
+++ packages/core/lib/services/game_model_service.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+
+import '../models/game_model.dart';
+import '../models/game_model_parser.dart';
+
+class GameModelService extends ChangeNotifier {
+  GameModel? _activeModel;
+  GameModel? get activeModel => _activeModel;
+  bool get isLoaded => _activeModel != null;
+
+  Future<void> loadFromAsset(String assetPath) async {
+    try {
+      final jsonString = await rootBundle.loadString(assetPath);
+      _activeModel = GameModelParser.parse(jsonString);
+      notifyListeners();
+    } on FormatException catch (e) {
+      debugPrint('GameModelService: failed to parse $assetPath: $e');
+    }
+  }
+}
--- packages/core/lib/services/services.dart
+++ packages/core/lib/services/services.dart
@@ -1,2 +1,3 @@
+export 'game_model_service.dart';
 export 'wiki_storage_service.dart';
 export 'wiki_search_service.dart';
